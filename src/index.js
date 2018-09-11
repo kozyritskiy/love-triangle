88d494fa01a6a41807f6d33e00fa9b697ecc5fcc
Max Kozyritsky <maxim.kozyricki@gmail.com>
2018-09-11 11:56:31 +0300

    first commit

--- src/index.js
+++ src/index.js
@@ -3,5 +3,25 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  let newArr = [];
+  preferences.unshift(0);
+  preferences.forEach(function(i) {
+    if (
+      preferences[i] !== preferences[preferences[i]] &&
+      preferences[preferences[i]] !== preferences[preferences[preferences[i]]] &&
+      preferences[preferences[preferences[i]]] !== preferences[i] &&
+      preferences[preferences[preferences[i]]] === i
+    ) {
+      newArr.push([
+        preferences[i],
+        preferences[preferences[i]],
+        preferences[preferences[preferences[i]]]
+      ]);
+      preferences[i] = 0;
+      preferences[preferences[i]] = 0;
+      preferences[preferences[preferences[i]]] = 0;
+    }
+  }) 
+  return newArr.length;
+
 };
